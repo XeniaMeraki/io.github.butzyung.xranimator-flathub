275e1f1e3ca64f2ddc89ec7e9b2be27046b4be7e
Xenia Meraki <xeniameraki@protonmail.com>
2025-12-02 14:03:01 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/sh
+##!/usr/bin/sh
 # exec /app/bin/zypak-wrapper "/app/bin/xranimator/XR Animator - electron-v33.2.0-linux-x64_SA/electron" "/app/bin/xranimator/AT_SystemAnimator_v110330.gadget/index.html" "$@"
-cd /app/bin/xranimator/xr || exit
-/app/bin/xranimator/xr/electron --no-sandbox "$@"
+#cd /app/bin/xranimator/xr || exit
+#/app/bin/xranimator/xr/electron --no-sandbox "$@"
+#!/bin/sh
+exec /app/bin/zypak-wrapper \
+    "/app/bin/xranimator/XR Animator - electron-v33.2.0-linux-x64_SA/electron" \
+    --ozone-platform-hint=auto \
+    --no-sandbox \
+    "/app/bin/xranimator/AT_SystemAnimator_v110330.gadget/index.html" \
+    "$@"
