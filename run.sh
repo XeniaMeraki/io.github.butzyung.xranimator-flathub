2b6af574b589df9953203b1ffeae6c7b3d59b8b1
Xenia Meraki <xeniameraki@protonmail.com>
2025-12-02 09:20:02 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-exec zypak-wrapper /app/bin/xranimator-electron/electron /app/share/xranimator/index.html "$@"
+exec zypak-wrapper /app/bin/xranimator/XR Animator - electron-v33.2.0-linux-x64_SA/electron /app/bin/xranimator/AT_SystemAnimator_v110330.gadget/index.html "$@"
